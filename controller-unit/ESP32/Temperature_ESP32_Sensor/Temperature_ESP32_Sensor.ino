a35cf689382a39adeaaee0e3fdb68ec3080b7b8a
Caner Emeç <42196875+CanerEmec@users.noreply.github.com>
2019-06-28 14:13:36 +0300

    Add "Deep Sleep" feature

--- controller-unit/ESP32/Temperature_ESP32_Sensor/Temperature_ESP32_Sensor.ino
+++ controller-unit/ESP32/Temperature_ESP32_Sensor/Temperature_ESP32_Sensor.ino
@@ -3,6 +3,8 @@
 #include <OneWire.h>
 #include <DallasTemperature.h>
 
+#define uS_TO_S_FACTOR 1000000  /* Conversion factor for micro seconds to seconds */
+#define TIME_TO_SLEEP  30        /* Time ESP32 will go to sleep (in seconds) */
 
 // Data wire is connected to GPIO13
 #define ONE_WIRE_BUS 13
@@ -13,17 +15,16 @@ OneWire oneWire(ONE_WIRE_BUS);
 // Pass our oneWire reference to Dallas Temperature sensor 
 DallasTemperature sensors(&oneWire);
 
-// Pass your Device Address..
-//DeviceAddress sensor = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };     //#1
+//DeviceAddress sensor = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };   //#1
 //DeviceAddress sensor = { <Your Device Address> };     //#2
-DeviceAddress sensor   = { <Your Device Address> };     //#3
-//DeviceAddress sensor = { <Your Device Address> };     //#4
+//DeviceAddress sensor = { <Your Device Address> };   //#3
+DeviceAddress sensor = { <Your Device Address> };   //#4
 
 
 
 // Replace the next variables with your SSID/Password combination
-const char* ssid = "<your ssid>";
-const char* password = "<your password>";
+const char* ssid = "<Your SSID>";
+const char* password = "<Your Password>";
 
 // Add your MQTT Broker IP address, example:
 const char* mqtt_server = "<Raspberry's IP Address>";
@@ -32,16 +33,42 @@ WiFiClient espClient;
 PubSubClient client(espClient);
 char temperature[8];
 
-void setup() {
+/*
+Method to print the reason by which ESP32
+has been awaken from sleep
+*/
+void print_wakeup_reason(){
+  esp_sleep_wakeup_cause_t wakeup_reason;
+
+  wakeup_reason = esp_sleep_get_wakeup_cause();
+
+  switch(wakeup_reason)
+  {
+    case ESP_SLEEP_WAKEUP_EXT0 : Serial.println("Wakeup caused by external signal using RTC_IO"); break;
+    case ESP_SLEEP_WAKEUP_EXT1 : Serial.println("Wakeup caused by external signal using RTC_CNTL"); break;
+    case ESP_SLEEP_WAKEUP_TIMER : Serial.println("Wakeup caused by timer"); break;
+    case ESP_SLEEP_WAKEUP_TOUCHPAD : Serial.println("Wakeup caused by touchpad"); break;
+    case ESP_SLEEP_WAKEUP_ULP : Serial.println("Wakeup caused by ULP program"); break;
+    default : Serial.printf("Wakeup was not caused by deep sleep: %d\n",wakeup_reason); break;
+  }
+}
+
+void setup(){
   Serial.begin(115200);
-  sensors.begin();
   
   setup_wifi();
   client.setServer(mqtt_server, 1883);
   client.setCallback(callback);
   
   pinMode(5, OUTPUT);
-}
+
+  //Print the wakeup reason for ESP32
+  print_wakeup_reason();
+
+  esp_sleep_enable_timer_wakeup(TIME_TO_SLEEP * uS_TO_S_FACTOR);
+  Serial.println("Setup ESP32 to sleep for every " + String(TIME_TO_SLEEP) + " Seconds");
+
+ }
 
 void setup_wifi() {
   delay(10);
@@ -63,13 +90,17 @@ void setup_wifi() {
   Serial.println(WiFi.localIP());
 }
 
+void callback(char* topic, byte* message, unsigned int length) {
+
+}
+
 void reconnect() {
   // Loop until we're reconnected
   while (!client.connected()) {
     Serial.print("Attempting MQTT connection...");
     
     // Attempt to connect
-    if (client.connect()) {
+    if (client.connect("ESP8266Client_1")) {
       Serial.println("connected");
       
     } else {
@@ -82,7 +113,8 @@ void reconnect() {
   }
 }
 
-void loop() {
+void loop(){
+  //This is not going to be called
   if (!client.connected()) {
     digitalWrite(5, HIGH);   
     delay(1000);                       
@@ -100,6 +132,8 @@ void loop() {
   Serial.println(temperature);
   
   client.publish("esp32/temperature_1",temperature);
-  delay(5000);
-    
+  
+  Serial.println("Going to sleep now");
+  Serial.flush(); 
+  esp_deep_sleep_start();  
 }
