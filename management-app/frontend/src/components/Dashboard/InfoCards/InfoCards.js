a7f78237b68af93bb2b6257b8442eb5650acc7a0
arslanhanbegum <arslanhanbegum@sabanciuniv.edu>
2019-07-05 08:24:45 +0300

    components updated

--- management-app/frontend/src/components/Dashboard/InfoCards/InfoCards.js
+++ management-app/frontend/src/components/Dashboard/InfoCards/InfoCards.js
@@ -1,12 +1,8 @@
 import React, {Component} from 'react'
 
 import {
-  Button,
-  ButtonGroup,
-  ButtonToolbar,
   Card,
   CardBody,
-  CardTitle,
   Col,
   Progress,
   Row,
@@ -16,19 +12,8 @@ import {
 class InfoCards extends Component{
     constructor(props){
         super(props)
-        this.state = {
-          selected : 1
-      }  
-  
     }
 
-    onRadioBtnClick(selected) {
-      this.setState({
-        selected: selected,
-      });
-      this.onRadioBtnClick = this.onRadioBtnClick.bind(this);
-    }
-    
     avgmodal() {
         var x =  (this.props.sensorTemp[1] +  this.props.sensorTemp[2] +  this.props.sensorTemp[3] + this.props.sensorTemp[4])/4
         x = x * 100   
@@ -43,38 +28,6 @@ class InfoCards extends Component{
         var y = a / x
         return y
       }
-
-      getPeople(){
-
-        if(this.state.selected == 1){
-       return( <CardBody className="pb-0">
-          <div className="text-value">   
-            <h4>People</h4>
-            <h2>{this.props.people}	</h2>
-          </div>
-          </CardBody>)
-        }
-        else if(this.state.selected == 2){
-          return(<CardBody className="pb-0">
-            <div className="text-value">   
-             <h4>Male</h4>
-               <h2>{this.props.male}</h2>
-            </div>
-            </CardBody>)
-          }
-          else if(this.state.selected == 3){
-            return(
-            <CardBody className="pb-0">
-            <div className="text-value">   
-             <h4>Female</h4>
-               <h2>{this.props.female}</h2>
-            </div>
-            </CardBody>)
-      
-          }
-      }
-      
-      
 render(){
     return(
         <Row className="text-center">
@@ -87,9 +40,9 @@ render(){
               <Progress className="progress-xs mt-2" color="primary" value={this.slack100(this.props.cold)} />
             </Col>
              <Col >
-              <Card style={{padding : '20px'}}>
+              <Card style={{padding : '41px'}}>
                 <CardBody className="pb-0">  
-                <div className="text-value"> <h4> OUTDOOR </h4>
+                <div   className="text-value"> <h4> OUTDOOR </h4>
                 <h2> {this.props.temp} °C </h2>
                 </div>
                 </CardBody>
@@ -97,21 +50,28 @@ render(){
             </Col>
             <Col >
             <Card>
-              <CardTitle>
-              <ButtonToolbar className="float-right" aria-label="Toolbar with button groups">
-                      <ButtonGroup aria-label="First group">
-                      <Button color="outline-secondary" onClick={() => this.onRadioBtnClick(1)} active={this.state.selected === 1}>People</Button>
-                        <Button color="outline-secondary" onClick={() => this.onRadioBtnClick(2)} active={this.state.selected === 2}>Male</Button>          
-                        <Button color="outline-secondary" onClick={() => this.onRadioBtnClick(3)} active={this.state.selected === 3}>Female</Button>                              
-                        </ButtonGroup>
-                    </ButtonToolbar>
-              </CardTitle>
-              {this.getPeople()}
-            
+              <CardBody className="pb-0">
+                <div className="text-value">
+                    <h4>People Count</h4></div>
+                    <h2>{this.props.people}	</h2>
+                <Row>
+                  <Col>
+                <div className="text-value">
+                    <h4>Male</h4></div>
+                    <h2> {this.props.male}	</h2>
+                </Col>
+                <Col>
+                <div className="text-value">
+                    <h4>Female</h4></div>
+                    <h2>{this.props.female}	</h2>
+                </Col>
+                </Row>
+              </CardBody>
+           
             </Card>
             </Col>
             <Col sm={12} md className="mb-sm-2 mb-0 d-md-down-none">
-              <Card style={{padding :'20px'}}>
+              <Card style={{padding : '41px'}}>
                 <CardBody className="pb-0">  
                   <div className="bg-transparent">
                   <h4> INDOOR </h4>
