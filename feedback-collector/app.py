268ab5c0f0124849b94578a52bf2e19c1b53815a
yunussevim <ahmetyunussevim@gmail.com>
2019-07-05 07:40:51 +0300

    Add files via upload

---
+++ feedback-collector/app.py
@@ -0,0 +1,202 @@
+from __future__ import print_function
+import sys
+import os
+from flask import Flask, render_template, g, request, make_response, Response
+import json
+import requests
+import slackMessages
+import databaseOperations
+from datetime import datetime
+from apscheduler.schedulers.background import BackgroundScheduler
+from slackclient import SlackClient
+from cryptography.fernet import Fernet
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'XYZ')
+
+db_conn = databaseOperations.connect_db()
+key = Fernet.generate_key()
+f = Fernet(key)
+scheduler = BackgroundScheduler()
+scheduler.add_job(func=databaseOperations.deleteAllSlashCommnads, args=(db_conn ,), trigger="interval", seconds=3600)
+scheduler.start()
+
+def decryptToken():
+    encrypted=databaseOperations.getToken(db_conn)
+    return encrypted
+def getUserList():
+    
+    sc = SlackClient(decryptToken())
+    usersID=sc.api_call(
+    "users.list")
+    userinfo={}
+    members=usersID["members"]
+    for i in range(len(members)):
+        useri=members[i]
+        userinfo[useri["name"]]=useri["id"]
+    return userinfo    
+
+def sendLocationSurveyOneUser(username):
+    
+    sc = SlackClient(decryptToken())
+    userinfo = getUserList()
+    userid=userinfo.get(username)
+    zones=databaseOperations.getACzones(db_conn)
+    sc.api_call(
+    "chat.postMessage",
+    channel=userid,
+    blocks=slackMessages.getLocationSurvey(zones)
+  
+)
+    return make_response("Success",200)
+
+def response_Interactive_Message(responseurl,text="Thanks :)"):
+    resdata = {
+    "replace_original": "true",
+    "text": text
+}
+    x = requests.post(responseurl,data=json.dumps(resdata))
+@app.route('/')
+def index():
+    return "App for Slack results (Services)"
+
+@app.route("/slack/oauth", methods=["GET"])
+def oauth():
+    client_id=os.environ["CLIENT_ID"]
+    client_secret=os.environ["CLIENT_SECRET"]
+    code = request.args.get("code")
+    uri= 'https://slack.com/api/oauth.access?code='+code+'&client_id='+client_id+'&client_secret='+client_secret  
+    
+    response=requests.post(uri)
+    response=json.loads(response.content)
+    print(response)
+    token=response["access_token"]
+    databaseOperations.setToken(db_conn,token,"admin")
+
+    return make_response("App Installed",200)
+
+@app.route("/slack/returns", methods=["POST"])
+def message_actions():
+    
+    form_json = json.loads(request.form["payload"])
+    print(form_json)
+    responseurl=form_json["response_url"]
+    user = form_json["user"]
+    username = user["username"]
+    userid= user["id"]
+    returnText="Success"
+    if "accessory" not in request.form["payload"]:
+        choosen= form_json["actions"]
+        choosen= choosen[0]
+        choosen = choosen["value"]
+        text = "Thanks for voting"
+        print(username)
+        print(choosen)
+        response_Interactive_Message(responseurl,text)
+        returnText= username+" "+choosen
+        databaseOperations.addVoteRecord(db_conn,username,choosen)
+    else:
+        actions=form_json["actions"]
+        actions=actions[0]
+        selectedoption=actions["selected_option"]
+        selectedValue=selectedoption["value"]
+        text = "Your location changed to " + selectedValue
+        response_Interactive_Message(responseurl,text)
+        print(username)
+        print(selectedValue)
+        returnText= username+" "+selectedValue
+        if(selectedValue not in "degismedi"):
+            databaseOperations.addLocationRecord(db_conn,username,selectedValue)
+    
+    
+    
+    return make_response(returnText,200)
+
+@app.route("/slack/airSurvey", methods=["POST"])
+def sendAirSurvey(creater="Auto"):
+    
+    sc = SlackClient(decryptToken())
+    userinfo = getUserList()
+    for val in userinfo.values():
+        sc.api_call(
+        "chat.postMessage",
+        channel=val,
+        blocks= slackMessages.getAirConSurvey()
+    
+)  
+    databaseOperations.addSurvey(db_conn,creater)
+    return make_response("Success",200)
+
+@app.route("/slack/locationSurvey", methods=["POST"])
+def sendLocationSurvey():
+    
+    sc = SlackClient(decryptToken())
+    userinfo = getUserList()
+    zones=databaseOperations.getACzones(db_conn)
+    for val in userinfo.values():
+        sc.api_call(
+        "chat.postMessage",
+        channel=val,
+        blocks=slackMessages.getLocationSurvey(zones)
+  
+)
+    
+    return make_response("Success",200)
+
+@app.route("/slack/slash", methods=["POST"])
+def collectSlashRequests():
+    username=request.form["user_name"]
+    command=request.form["command"]
+    print(username + "  " + command)
+    if(command=="/location"):
+        sendLocationSurveyOneUser(username)
+        return make_response("",200)
+    if(command=="/sendsurvey"):
+        sendAirSurvey("Manuel")
+        return make_response("",200)
+    if(command=="/locationall"):
+        sendLocationSurvey()
+        return make_response("",200)
+    if(command=="/onsurveyschedule"):
+        hour = request.form["text"]
+        
+        print(hour)
+        print(int(hour))
+        setSchedule(int(hour))
+        return make_response("Survey schedule setted to "+ hour+" hour",200)
+        
+        #return make_response("Please enter proper hour",200)
+    if(command=="/offsurveyschedule"):
+        removeSchedule()
+        return make_response("Survey schedule stopped",200)
+    if(command=="/hot" or command == "/cold"):
+        location = databaseOperations.getPersonLocation(db_conn,username)
+        
+        if(location==False):
+            return make_response("Your location not found.",200)
+        
+        ret=databaseOperations.addSlashData(db_conn,username,location,command)
+        return make_response(ret,200)
+    else:
+        return make_response("This command is not exist.",200)
+@app.route('/slack/setSchedule', methods=["POST"])
+def setSchedule(hourSlash):
+    hour = request.form.get("hour")
+    print("schedule setted "+str(hour)+ " hours")
+    scheduler.add_job(id="surveyschedule", func=sendAirSurvey, args=("Manuel",) ,trigger="interval", seconds=int(hourSlash)*3600)
+    print("schedule added")
+    return make_response("Success",200)
+
+@app.route('/slack/removeSchedule', methods=["POST"])
+def removeSchedule():
+    scheduler.remove_job("surveyschedule")
+    print("Schedule removed")
+    return make_response("Success",200)
+
+@app.route("/slack/heya", methods=["POST"])
+def retret():
+    
+    return make_response("Success",200)
+
+if (__name__ == '__main__'):
+   app.run()
---
+++ feedback-collector/databaseOperations.py
@@ -0,0 +1,178 @@
+from __future__ import print_function
+import sys
+import psycopg2
+import os
+from datetime import datetime
+def connect_db():
+   return psycopg2.connect(os.environ.get('DATABASE_URL'))
+
+def addVoteRecord(connection,username,vote):
+    userid=isUserExist(connection,username,"PersonalInfo")
+    if not(userid):
+        return False
+    userid=userid[0]
+    userid=userid[0]
+    cursor = connection.cursor()
+    voteid=int(getLastSurveyID(connection))
+    postgres_insert_query = """ INSERT INTO weatherpoll (user_id, vote, vote_id, data_time) VALUES (%s,%s,%s,%s)"""
+    record_to_insert = (userid,vote,voteid   ,datetime.now())
+    cursor.execute(postgres_insert_query, record_to_insert)
+    connection.commit()
+    count = cursor.rowcount
+    print (count, "Record inserted successfully into weatherpoll table")
+    cursor.close()
+    return True
+    
+def addLocationRecord(connection,username,location):
+    cursor = connection.cursor()
+    location=int(location)
+    postgres_insert_query = """ INSERT INTO personalInfo (ac_id,username,birthday) VALUES (%s,%s,%s)"""
+    postgres_update_query = """Update personalInfo set ac_id = %s where username = %s"""
+    
+    if(isUserExist(connection,username,"personalInfo")):
+        
+        cursor.execute(postgres_update_query, (location, username))
+        connection.commit()
+        
+    else:
+        
+        print(type(location))
+        record_to_insert = (location,username,datetime.now())
+        cursor.execute(postgres_insert_query,record_to_insert)
+        connection.commit()
+    cursor.close()
+    return True
+
+def isUserExist(connection,username,dbTableName):
+    cursor = connection.cursor()
+    postgres_select_query=""" Select ID from """+dbTableName+""" where username = '"""+str(username)+"'"
+    print(postgres_select_query)
+    cursor.execute(postgres_select_query)
+    person=cursor.fetchall()
+    print(person)
+    cursor.close()
+    if person is None:
+        return False
+    else:
+        return person
+
+def getACzones(connection):
+    ids=[]
+    cursor=connection.cursor()
+    postgres_select_query=""" Select ac_id from AC """
+    cursor.execute(postgres_select_query)
+    zones=cursor.fetchall()
+    cursor.close()
+    for a in zones:
+        b=a[0]
+        ids.append(b)
+    print(ids)
+    return ids
+
+def getPersonLocation(connection,username):
+    cursor= connection.cursor()
+    postgres_select_query=""" Select ac_id from personalInfo where username= '"""+str(username)+"'"
+    cursor.execute(postgres_select_query)
+    location=cursor.fetchone()
+    cursor.close()
+    if location!=None:
+        return location[0]
+    else:
+        return False
+
+def addSurvey(connection,author):
+    cursor = connection.cursor()
+    postgres_insert_query="""Insert into survey (creater,data_time) VALUES (%s,%s)"""
+    record_to_insert = (author,datetime.now())
+    cursor.execute(postgres_insert_query, record_to_insert)
+    connection.commit()
+    cursor.close()
+
+def addSlashData(connection,username,location,command):
+    command=command[1:len(command)]
+    if(isUserExist(connection,username,"slack_temp")):
+        return "You have already sent command."
+    cursor = connection.cursor()
+    postgres_insert_query="""Insert into slack_temp (username,loc,comment)VALUES (%s,%s,%s)"""
+    record_to_insert = (username,location,command)
+    cursor.execute(postgres_insert_query, record_to_insert)
+    connection.commit()
+    cursor.close()
+    return "We will take care of it."
+
+def checkSlashData(connection):
+    cursor=connection.cursor()
+    postgres_select_query=""" Select * from slack_temp"""
+    cursor.execute(postgres_select_query)
+    records=cursor.fetchall()
+    cursor.close()
+    zones = getACzones(connection)
+    arr=[]
+    for _ in range(len(zones)):
+        arr.append([0,0])
+    print(arr)
+
+    for record in records:
+        username = record[1]
+        loc = int(record[2])
+        command = record[3]
+        loc -=1
+        print(username+ " "+str(loc)+" "+command)
+        if(command=="cold"):
+            arr[loc][1]+=1
+            print(arr)
+            if(arr[loc][1]>2):
+                deleteSlashCommands(connection,loc+1,command)
+                arr[loc][1]=0
+                #bildirim at
+        if(command=="hot"):
+            arr[loc][0]+=1
+            if(arr[loc][0]>2):
+                deleteSlashCommands(connection,loc+1,command)
+                arr[loc][0]=0
+                #bildirim at
+        
+    print(arr)
+
+def deleteSlashCommands(connection,location,command):
+    location = str(location)
+    cursor=connection.cursor()
+    postgres_delete_query="delete from slack_temp where loc="+location+" AND comment='"+command+"'"
+    cursor.execute(postgres_delete_query)
+    connection.commit()
+    print(command +" commands at" +location + " are deleted")
+    cursor.close()
+
+def deleteAllSlashCommnads(connection):
+    cursor=connection.cursor()
+    postgres_delete_query="delete from slack_temp"
+    cursor.execute(postgres_delete_query)
+    connection.commit()
+    print("All commands are deleted")
+    cursor.close()
+
+def getLastSurveyID(connection):
+    cursor=connection.cursor()
+    postgres_select_query="SELECT id FROM survey ORDER BY data_time DESC LIMIT 1"
+    cursor.execute(postgres_select_query)
+    voteid=cursor.fetchone()
+    voteid=voteid[0]
+    print(voteid)
+    return voteid
+
+def getToken(connection):
+    cursor=connection.cursor()
+    postgres_select_query="SELECT token FROM systemadmin LIMIT 1"
+    cursor.execute(postgres_select_query)
+    token=cursor.fetchone()
+    token=token[0]
+    return token
+
+def setToken(connection,token,username):
+    cursor = connection.cursor()
+    postgres_update_query = """Update systemadmin set token = %s where username = %s """
+    cursor.execute(postgres_update_query,(token,username))
+    connection.commit()
+    cursor.close()
+    return True
+        
