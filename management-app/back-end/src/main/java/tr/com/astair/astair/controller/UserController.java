c5fce3fa81d6622484a597c7e46922415fd50764
Burak Yasar <burakyasar97@gmail.com>
2019-07-08 15:12:41 +0300

    delete old comment outed lines

--- management-app/back-end/src/main/java/tr/com/astair/astair/controller/UserController.java
+++ management-app/back-end/src/main/java/tr/com/astair/astair/controller/UserController.java
@@ -1,55 +1,3 @@
-/*
-package tr.com.astair.astair.controller.api;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import tr.com.astair.astair.controller.UserControllerApi;
-import tr.com.astair.astair.model.User;
-import tr.com.astair.astair.service.UserService;
-
-@RestController
-public class UserController implements UserControllerApi {
-
-    private UserService userService;
-
-    @Autowired
-    public UserController(UserService userService) {
-        this.userService = userService;
-    }
-
-    public ResponseEntity<User> save(@RequestBody User user) {
-        try{
-           user = userService.save(user);
-            return new ResponseEntity<>(user, HttpStatus.OK);
-        }catch (Exception ex){
-            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-    }
-
-
-    public ResponseEntity<User> getByID(@PathVariable Long id){
-        User user = userService.get(id);
-        if(user == null){
-            return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
-        }
-        return new ResponseEntity<>(user, HttpStatus.OK);
-    }
-
-
-
-    public ResponseEntity<User> update(@RequestBody User user){
-        if(user.getId() ==null || userService.get(user.getId()) == null)
-            return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
-        userService.update(user);
-        return  new ResponseEntity<>(null, HttpStatus.OK);
-
-    }
-}
-*/
-
-
 package tr.com.astair.astair.controller;
 
 import tr.com.astair.astair.controller.api.UserControllerApi;
@@ -61,9 +9,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.*;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.web.bind.annotation.RequestMapping;
-
 
 @RestController(value="/user")
 public class UserController implements UserControllerApi {
--- management-app/back-end/src/main/java/tr/com/astair/astair/repository/UserRepository.java
+++ management-app/back-end/src/main/java/tr/com/astair/astair/repository/UserRepository.java
@@ -1,12 +1,3 @@
-/*
-package tr.com.astair.astair.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import tr.com.astair.astair.model.User;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
-*/
 package tr.com.astair.astair.repository;
 import tr.com.astair.astair.model.User;
 import java.util.List;
