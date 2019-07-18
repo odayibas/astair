784caaa9886c0c58648223346a417d140ddb55c3
Burak Yasar <burakyasar97@gmail.com>
2019-07-18 11:26:29 +0300

    MQTT protocol added to spring boot

---
+++ management-app/back-end/src/main/java/tr/com/astair/astair/controller/MqttController.java
@@ -0,0 +1,58 @@
+package tr.com.astair.astair.controller;
+
+import tr.com.astair.astair.config.Mqtt;
+import tr.com.astair.astair.exception.ExceptionMessages;
+import tr.com.astair.astair.exception.MqttException;
+import tr.com.astair.astair.model.MqttPublishModel;
+import tr.com.astair.astair.model.MqttSubscribeModel;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+@RestController
+@RequestMapping(value = "/api/mqtt")
+public class MqttController {
+
+    @PostMapping("publish")
+    public void publishMessage(@RequestBody @Valid MqttPublishModel messagePublishModel,
+                               BindingResult bindingResult) throws org.eclipse.paho.client.mqttv3.MqttException {
+        if (bindingResult.hasErrors()) {
+            throw new MqttException(ExceptionMessages.SOME_PARAMETERS_INVALID);
+        }
+
+        MqttMessage mqttMessage = new MqttMessage(messagePublishModel.getMessage().getBytes());
+        mqttMessage.setQos(messagePublishModel.getQos());
+        mqttMessage.setRetained(messagePublishModel.getRetained());
+
+       // Mqtt.getInstance().publish(messagePublishModel.getTopic(), mqttMessage);
+        Mqtt.getInstance().publish("webapp", new MqttMessage("Done".getBytes()));
+    }
+
+    @GetMapping("subscribe")
+    public List<MqttSubscribeModel> subscribeChannel(@RequestParam(value = "Astair/+/AC/CONF/GET") String topic,
+                                                     @RequestParam(value = "wait_millis") Integer waitMillis)
+            throws InterruptedException, org.eclipse.paho.client.mqttv3.MqttException {
+        List<MqttSubscribeModel> messages = new ArrayList<>();
+        CountDownLatch countDownLatch = new CountDownLatch(10);
+        Mqtt.getInstance().subscribeWithResponse(topic, (s, mqttMessage) -> {
+            MqttSubscribeModel mqttSubscribeModel = new MqttSubscribeModel();
+            mqttSubscribeModel.setId(mqttMessage.getId());
+            mqttSubscribeModel.setMessage(new String(mqttMessage.getPayload()));
+            mqttSubscribeModel.setQos(mqttMessage.getQos());
+            messages.add(mqttSubscribeModel);
+            countDownLatch.countDown();
+        });
+
+        countDownLatch.await(waitMillis, TimeUnit.MILLISECONDS);
+
+        return messages;
+    }
+
+
+}
---
+++ management-app/back-end/src/main/java/tr/com/astair/astair/model/MqttSubscribeModel.java
@@ -0,0 +1,33 @@
+package tr.com.astair.astair.model;
+
+
+public class MqttSubscribeModel {
+
+    private String message;
+    private Integer qos;
+    private Integer id;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Integer getQos() {
+        return qos;
+    }
+
+    public void setQos(Integer qos) {
+        this.qos = qos;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+}
