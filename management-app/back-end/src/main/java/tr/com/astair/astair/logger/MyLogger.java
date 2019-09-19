d283869c7750a53f800c796734516ae992ef5a5e
mdemiroz1 <murat.sady31@gmail.com>
2019-09-19 16:17:04 +0300

    logger now flushes the buffer before printing.

--- management-app/back-end/src/main/java/tr/com/astair/astair/logger/MyLogger.java
+++ management-app/back-end/src/main/java/tr/com/astair/astair/logger/MyLogger.java
@@ -12,7 +12,7 @@ public class MyLogger {
             String nameofCallerMethod = new Throwable().getStackTrace()[1].getMethodName();
             File f = new File("./log.txt");
             FileWriter fw = new FileWriter(f, true);
-            PrintWriter pw = new PrintWriter(fw);
+            PrintWriter pw = new PrintWriter(fw, true);
             pw.printf("Tarih: %s Class: %s Method: %s Message: %s Thread ID: %d\n",
                     new Date(), o.getClass().getName(), nameofCallerMethod, message, Thread.currentThread().getId());
             pw.close();
