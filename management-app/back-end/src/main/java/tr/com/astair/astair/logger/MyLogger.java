093db217146817915e72ab4e3aa20aa0c21b5216
mdemiroz1 <murat.sady31@gmail.com>
2019-09-20 09:32:04 +0300

    Artık alınan warning'e rağmen ekrana düzgün bir şekilde basıyor.

--- management-app/back-end/src/main/java/tr/com/astair/astair/logger/MyLogger.java
+++ management-app/back-end/src/main/java/tr/com/astair/astair/logger/MyLogger.java
@@ -2,6 +2,7 @@ package tr.com.astair.astair.logger;
 
 import java.util.*;
 import java.io.*;
+import java.util.logging.Logger;
 
 public class MyLogger {
 
@@ -9,12 +10,19 @@ public class MyLogger {
 
         try{
             String nameofCallerMethod = new Throwable().getStackTrace()[1].getMethodName();
+            long thread_id = Thread.currentThread().getId();
+
             File f = new File("./log.txt");
             FileWriter fw = new FileWriter(f, true);
             PrintWriter pw = new PrintWriter(fw, true);
             pw.printf("Tarih: %s Class: %s Method: %s Message: %s Thread ID: %d\n",
-                    new Date(), o.getClass().getName(), nameofCallerMethod, message, Thread.currentThread().getId());
+                    new Date(), o.getClass().getName(), nameofCallerMethod, message, thread_id);
             pw.close();
+
+            Logger logger = Logger.getLogger(o.getClass().getName());
+            logger.info("Tarih" + new Date() + " Class: " + o.getClass().getName() + " Method: " + nameofCallerMethod
+                        + "Message: " + message + " Thread ID: " + thread_id);
+
         } catch(IOException e){e.printStackTrace();}
 
     }
