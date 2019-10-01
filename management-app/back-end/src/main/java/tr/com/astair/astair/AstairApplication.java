5ccbc108ae4a25208e4ccbad9778f186d9ebe809
ahmetserdargurbuz <ahmetserdargurbuz@gmail.com>
2019-10-01 11:14:57 +0300

    Revert "2 new annotations added."
    This reverts commit 391f8b3d8c9a265af927c2cdbba114728aa1287c.

--- management-app/back-end/src/main/java/tr/com/astair/astair/AstairApplication.java
+++ management-app/back-end/src/main/java/tr/com/astair/astair/AstairApplication.java
@@ -2,15 +2,11 @@ package tr.com.astair.astair;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.client.RestTemplate;
 
 
 @SpringBootApplication
-@ComponentScan()
-@EntityScan()
 public class AstairApplication {
 
     public static void main(String[] args) {
